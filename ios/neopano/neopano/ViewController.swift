b8475fa8ac4042e53c2771e350b18d93ea686187
shunta nakajima <nakg_asimo@icloud.com>
2019-11-25 12:15:18 +0900

    minimum fix

---
+++ ios/neopano/neopano/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  neopano
+//
+//  Created by shunta nakajima on 2019/11/25.
+//  Copyright © 2019 Shunta Nakajima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var webView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let url = URL(string : "http://192.168.137.33:3001")
+        let urlRequest = URLRequest.init(url: url!)
+        self.webView.loadRequest(urlRequest)
+    }
+
+
+}
+
